8bc2c542cc446c77ed44ad222c3e14bb858eaff8
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-02-10 15:34:21 +0100

    interactive searchbar working

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,58 +2,89 @@ import React, { useState, Dispatch, SetStateAction, Component } from "react";
 import { TezosToolkit } from "@taquito/taquito";
 import { Contract, ContractsService,DefaultApiOptions } from '@dipdup/tzkt-api';
 import { render } from "react-dom";
-import { Box, Button, Card, CardContent, CardMedia, Chip, Slider, TextField, Typography } from "@mui/material";
+import { Autocomplete, Box, Button, Card, CardContent, CardMedia, Chip, CircularProgress, Slider, TextField, Typography } from "@mui/material";
 import { EmojiEvents, PersonOutlined, RecordVoiceOver } from "@mui/icons-material";
 import { Mark } from "@mui/base";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 
-class Result {
-  key : string;
-  value : number;
-  constructor(key : string,
-    value : number){
-      this.key = key;
-      this.value = value;
-    }
+const Search = ({
+  Tezos,
+  userAddress
+}: {
+  Tezos: TezosToolkit;
+  userAddress: string;
+}): JSX.Element => {
+  
+  enum STATUS {
+    ONGOING = "ONGOING",
+    FINISHED = "FINISHED"
   }
   
-  const Search = ({
-    Tezos,
-    userAddress
-  }: {
-    Tezos: TezosToolkit;
-    userAddress: string;
-  }): JSX.Element => {
-
-    enum STATUS {
-      ONGOING = "ONGOING",
-      FINISHED = "FINISHED"
+  //SEARCH
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = useState<Array<string>>([]);
+  const loading = open && options.length === 0;
+  const [inputValue, setInputValue] = React.useState<string>('');
+  const [value, setValue] = React.useState<string|null>(null);
+  
+  
+  //LIST
+  const [contracts, setContracts] = useState<Array<Contract>>([]);
+  
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
     }
+  }, [open]);
+  
+  React.useEffect(() => {
 
-    const [nameFilter, setNameFilter] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
-    const [contracts, setContracts] = useState<Array<Contract>>([]);
+    console.log("React.useEffect");
+
+    let active = true;
+    
+    if (inputValue === '') {
+      setOptions(value ? [value] : []);
+      console.log("input is null");
+      return undefined;
+    }
+    
+    (async () => {
+      console.log("Searching contracts...");
+      
+      setContracts((await contractsService.getSame({address:"KT1PYJvdStoHsCsNoKTFigqCqjd5eWo1uMYd" , includeStorage:true, sort:{desc:"id"}}))
+      .filter((c: Contract, index: number, array: Contract[]) => {
+        return (c.storage.name as string).search(new RegExp(inputValue, 'gi')) >= 0}
+        )); 
+        
+        console.log(contracts);
+        if (active) {
+          let newOptions: Array<string> = [];
+
+          
+          if (contracts) {
+            newOptions = [...newOptions, ...contracts.map((c: Contract, index: number, array: Contract[]) => {return c.storage.name;})];
+          }
+
+          if (value && options.indexOf(value) === -1) {
+            newOptions = [...newOptions,value];
+          }
+          
+          setOptions(newOptions);
+        }else{
+          console.log("not active");
+        }
+      })();
+      
+      return () => {
+        console.log("returning");
+        active = false;
+      };
+    }, [value, inputValue, loading]);
     
     const contractsService = new ContractsService( {baseUrl: "https://api.hangzhou2net.tzkt.io" , version : "", withCredentials : false});
     
-    const search = async (nf : string): Promise<void> => {
-      setNameFilter(nf);
-      if (nameFilter) {
-        setLoading(true);
-        try {
-          
-          setContracts(await contractsService.getSame({address:"KT1PYJvdStoHsCsNoKTFigqCqjd5eWo1uMYd" , includeStorage:true}));
-          console.log(contracts);
-          
-          
-          
-        } catch (error) {
-          console.log(error);
-        } finally {
-          setLoading(false);
-        }
-      }
-    };
+    
     
     const dateSliderToString = (value : number,index : number) : string =>{
       return new Date(value*1000000000000).toLocaleString();
@@ -120,54 +151,80 @@ class Result {
       }
     };
     
-    
     return (
       <div>
-      <div id="search-inputs">
-      <input
-      type="text"
-      placeholder="name"
-      value={nameFilter}
-      onChange={e => search(e.target.value)}
-      />
-      </div>
       
-      {contracts.map((contract, index) => (
-        <Card key={contract.address} sx={{ display: 'flex' }}>
-        <Box width="70%" sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-        <Typography component="div" variant="h5">
-        <a
-        href={`https://hangzhou2net.tzkt.io/${contract.address}/info`}
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        {contract.storage.name}
-        </a>
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary" component="div">
-        Created by <PersonOutlined /> {contract.creator?.address}
-        </Typography>
-        {buttonChoices(contract)}
-        </CardContent>
+      <Autocomplete
+      id="searchInput"
+      freeSolo
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
+      sx={{ width: "90%" }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      isOptionEqualToValue={(option, value) => option === value}
+      getOptionLabel={(option) => option}
+      options={options}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField {...params} label="Type a question here ..." fullWidth />
+        )}
+        onInputChange={(event, newInputValue) => {
+          console.log("onInputChange:"+newInputValue);
+          setInputValue(newInputValue);
+        }}
+        onChange={(event, newValue) => {
+          console.log("onChange:"+newValue);
+          setOptions(newValue && options.indexOf(newValue) === -1 ? [newValue, ...options] : options);
+          setValue(newValue);
+        }}
         
-        </Box>
-        <Box paddingTop="1em" paddingRight="5em" paddingLeft="5em" width="20%">
-        {resultArea(contract)}
-        </Box>
-        <Box padding="1em" height="auto" width="10%">
-        <CardMedia
-        component="img"
-        height="auto"
-        image="https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg"
+        
         />
-        </Box>
-        </Card>
-        ))}
         
-        </div>
-        );
-      };
-      
-      export default Search;
-      
+        {contracts.map((contract, index) => (
+          <Card key={contract.address} sx={{ display: 'flex' }}>
+          <Box width="70%" sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+          <a
+          href={`https://hangzhou2net.tzkt.io/${contract.address}/info`}
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          {contract.storage.name}
+          </a>
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+          Created by <PersonOutlined /> {contract.creator?.address}
+          </Typography>
+          {buttonChoices(contract)}
+          </CardContent>
+          
+          </Box>
+          <Box paddingTop="1em" paddingRight="5em" paddingLeft="5em" width="20%">
+          {resultArea(contract)}
+          </Box>
+          <Box padding="1em" height="auto" width="10%">
+          <CardMedia
+          component="img"
+          height="auto"
+          image="https://static8.depositphotos.com/1009634/988/v/950/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg"
+          />
+          </Box>
+          </Card>
+          ))}
+          
+          </div>
+          );
+        };
+        
+        export default Search;
+        
