5ed44cbefbb7285ceed30839eb599c573c842e78
Felipe Cardozo <fraifelipe@gmail.com>
2022-02-28 11:53:41 -0300

    cronjob: compare if value has changed first before updating the storage of the oracle

--- cronjob/index.ts
+++ cronjob/index.ts
@@ -1,45 +1,54 @@
 import { MichelsonMap, TezosToolkit } from "@taquito/taquito";
 import { InMemorySigner } from "@taquito/signer";
 import { config } from "dotenv";
-import { MichelsonV1Expression } from "@taquito/rpc";
+import BigNumber from "bignumber.js";
+
+type OracleStorage = {
+  votingPeriodIndexes: MichelsonMap<string, BigNumber>,
+  admin: string
+};
 
 // Set's up our environment variables from the file .env
 config();
 
-const TEZOS_SECRET_KEY = process.env.TEZOS_SECRET_KEY!;
 const TEZOS_RPC_URL = process.env.TEZOS_RPC_URL!;
 const TEZOS_RPC_ADDRESS = process.env.TEZOS_RPC_ADDRESS!;
-const TIME = parseInt(process.env.TIME!);
+const TEZOS_SECRET_KEY = process.env.TEZOS_SECRET_KEY!;
+const TEZOS_NETWORK = process.env.TEZOS_NETWORK || "mainnet"
+const TIME = parseInt(process.env.TIME) || 15000;
+const AMMOUNT = parseInt(process.env.AMMOUNT) || 1;
+const VALUE = parseInt(process.env.VALUE) || 27;
+const CONFIRMATION = parseInt(process.env.CONFIRMATION) || 1;
 
 const Tezos = new TezosToolkit(TEZOS_RPC_URL);
 
 Tezos.setProvider({ signer: new InMemorySigner(TEZOS_SECRET_KEY) });
 
-// Get Money by address endpoint
-const address = TEZOS_RPC_ADDRESS;
-
-// Send an arbitrary amount
-const amount = 1;
-
 const job = async () => {
-  const michelsonV1Expression: MichelsonV1Expression = {
-    prim: 'Right',
-    args: [{ prim: 'Pair', args: [{ string: "hangzhounet" }, { int: "27" }] }],
+  const response = await Tezos.rpc.getCurrentPeriod();
+  const period = response.voting_period.index;
+  const contract = await Tezos.wallet.at(TEZOS_RPC_ADDRESS);
+  const storage = await contract.storage<OracleStorage>();
+  const currentValue = storage.votingPeriodIndexes.get(TEZOS_NETWORK)
+
+  if (currentValue.toString() != period.toString()) {
+    console.log(`The current value (${currentValue.toString()}) is NOT the same that the voting period value (${period.toString()})!`);
+    console.log(`Transfering ${AMMOUNT} ꜩ to ${TEZOS_RPC_ADDRESS}...`);
+
+    const transfer = contract.methods.updateCurrentVotingPeriod(TEZOS_NETWORK, VALUE)
+      .toTransferParams({ amount: AMMOUNT });
+
+    const op = await Tezos.contract.transfer(transfer)
+
+    console.log(`Waiting for ${op.hash} to be confirmed...`);
+    await op.confirmation(CONFIRMATION);
+
+    console.log(`Confirmed - ${op.hash} - Funds transferred!`);
+    console.log(`Check url for results: https://${TEZOS_NETWORK}.tzkt.io/${op.hash}\n`);
+  } else {
+    console.log(`The current value (${currentValue.toString()}) is the same that the voting period value (${period.toString()})!`);
+    console.log(`Transection skipped!`);
   }
-
-  console.log(`Transfering ${amount} ꜩ to ${address}...`);
-  const op = await Tezos.contract.transfer(
-    {
-      to: address, amount: amount,
-      parameter: { entrypoint: "default", value: michelsonV1Expression }
-    }
-  );
-
-  console.log(`Waiting for ${op.hash} to be confirmed...`);
-  await op.confirmation(1);
-
-  console.log(`Confirmed - ${op.hash} - Funds transferred!`);
-  console.log(`Check url for results: https://hangzhounet.tzkt.io/${op.hash}\n`);
 }
 
 const cron = () => new Promise(resolve => setTimeout(resolve, TIME));
