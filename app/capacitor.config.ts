8ec1f3543ae12d10ae362a5f3268a49960681a92
Benjamin Fuentes <zamrokk@gmail.com>
2023-09-07 09:34:07 +0200

    updating metadata

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app',
-  webDir: 'dist',
+  appId: "dev.marigold.tzvote",
+  appName: "TzVote",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
